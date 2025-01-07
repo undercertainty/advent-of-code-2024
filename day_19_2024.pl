b58d7da017808defdfdbe359b3db6c21fcf832d4
undercertainty <undercertainty@gmail.com>
2025-01-07 11:13:38 +0000

    Day 19 complete

---
+++ day_19_2024.pl
@@ -0,0 +1,142 @@
+%
+% Looks perfect Prolog fodder...
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Library imports
+
+:- use_module(library(lists)).
+:- use_module(library(readutil)).
+:- use_module(library(dcg/basics)).
+:- use_module(library(yall)).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% AoC boilerplate
+
+aoc_19_test_part1(Out):-
+    solve_aoc_19_part1("data/data_19_2024_test.txt", Out).
+
+aoc_19_part1(Out):-
+    solve_aoc_19_part1("data/data_19_2024.txt", Out).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Today's stuff
+
+solve_aoc_19_part1(FileName, Out):-
+    get_input(FileName, (Towels, Designs)),!,
+    include([X]>>valid_design(X, Towels), Designs, ValidDesigns),
+    length(ValidDesigns, Out).
+
+get_input(FileName, Out):-
+    read_file_to_string(FileName, FileString, []),
+    string_codes(FileString, Codes),
+    phrase(day19(Out), Codes).
+
+
+day19((Towels, Designs)) --> towels(Towels),
+                             eol,
+                             eol,
+                             designs(Designs).
+
+towels([Towel|Towels]) --> blanks,string(TowelCodes),
+                                ",",
+                                towels(Towels),
+                                {atom_codes(TowelAtom, TowelCodes),
+                                 atom_chars(TowelAtom, Towel)}.
+towels([Towel]) --> blanks,string(TowelCodes),
+                    {atom_codes(TowelAtom, TowelCodes),
+                                 atom_chars(TowelAtom, Towel)}.
+
+designs([Design]) --> nonblanks(DesignCodes),
+                     {atom_codes(DesignAtom, DesignCodes),
+                                 atom_chars(DesignAtom, Design)}.
+designs([Design|Designs]) --> nonblanks(DesignCodes),
+                              eol,
+                              designs(Designs),
+                               {atom_codes(DesignAtom, DesignCodes),
+                                 atom_chars(DesignAtom, Design)}.
+
+valid_design([], _).
+valid_design(Design, Towels):-
+    member(Towel, Towels),
+    append(Towel, DesignRest, Design),
+    valid_design(DesignRest, Towels).
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%%% Part 2:
+%
+% I have to confess that I was most of the way
+% there with this part, but needed a nudge from
+% reddit to get the algorithm quite right.
+% Annoyed with myself, 'cos this sort of dynamic
+% programming task is classic traditional
+% parsing stuff.
+
+aoc_19_test_part2(Out):-
+    solve_aoc_19_part2("data/data_19_2024_test.txt", Out).
+
+aoc_19_part2(Out):-
+    solve_aoc_19_part2("data/data_19_2024.txt", Out).
+
+%%%%%%%%
+
+solve_aoc_19_part2(FileName, Out):-
+    get_input(FileName, (Towels, Designs)),!,
+    maplist([X, Y]>>count_arrangements(X, Towels, Y), Designs, Arrangements),
+    sumlist(Arrangements, Out).
+
+
+%%% Well findall blows the stack, but thinking about it,
+%%% it's just a chart parser...
+
+
+count_arrangements(Design, Towels, Out):-
+    get_edges(Design, Towels, Edges),
+    !,
+    length(Design, DesignLength),
+    build_chart(Edges, DesignLength, Chart),
+    last(Chart, Out).
+
+% setof isn't very efficient, but it's not the
+% bottleneck.
+
+get_edges(Design, Towels, Edges):-
+    setof(Span, edge(Design, Towels, Span), Edges).
+
+edge(Design, Towels, edge(Start, End, Towel)):-
+    member(Towel, Towels),
+    append(DesignL, _DesignR, Design),
+    append(LL, Towel, DesignL),
+    length(LL, Start),
+    length(Towel, TL),
+    End is Start + TL.
+
+
+build_chart(Edges, ChartLength, Chart):-
+    length(EmptyChart, ChartLength),
+    maplist(=(0), EmptyChart),
+    build_chart(Edges, 1, [1|EmptyChart], Chart).
+
+build_chart([], _, Chart, Chart).
+build_chart([Edge|Edges], N, ChartSoFar, ChartOut):-
+    select(edge(Start, N, Towel), [Edge|Edges], EdgesNext),
+    add_edge(edge(Start, N, Towel), ChartSoFar, ChartNext),
+    build_chart(EdgesNext, N, ChartNext, ChartOut).
+build_chart([Edge|Edges], N, ChartSoFar, ChartOut):-
+    \+ member(edge(_Start, N, _Towel), [Edge|Edges]),
+    succ(N, N1),
+    build_chart([Edge|Edges], N1, ChartSoFar, ChartOut).
+
+add_edge(edge(Start, End, _Towel), ChartIn, ChartOut):-
+    nth0(Start, ChartIn, StartCount),
+    length(ChartLeft, End),
+    append(ChartLeft, [EndCount|ChartRight], ChartIn),
+    EndCount1 is StartCount + EndCount,
+    append(ChartLeft, [EndCount1|ChartRight], ChartOut).
